b80fe35847529bb77e541e97dd80090fb23e2c17
Matheus Miura <matheus.miura@luminiitsolutions.com>
2026-02-03 18:39:50 -0300

    teste

--- SecretariaIa.Api/Program.cs
+++ SecretariaIa.Api/Program.cs
@@ -15,6 +15,11 @@ builder.Services.AddSwagger<SwaggerFillter>(builder.Configuration);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 var app = builder.Build();
 
+Console.WriteLine("OPENAI KEY: " + (Environment.GetEnvironmentVariable("OpenAI__ApiKey") != null));
+Console.WriteLine("TWILIO SID: " + (Environment.GetEnvironmentVariable("Twilio__AccountSid") != null));
+Console.WriteLine("TWILIO TOKEN: " + (Environment.GetEnvironmentVariable("Twilio__AuthToken") != null));
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
