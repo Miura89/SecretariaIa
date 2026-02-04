8cdbc4b841130c6492dfc35743cd47ca6af17ff3
Matheus Miura <matheus.miura@luminiitsolutions.com>
2026-02-04 10:38:42 -0300

    voltar

--- SecretariaIa.Infrasctructure/Data/Services/TwilioWhatsAppSender.cs
+++ SecretariaIa.Infrasctructure/Data/Services/TwilioWhatsAppSender.cs
@@ -2,9 +2,8 @@
 using SecretariaIa.Common.Interfaces;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
-using Twilio.TwiML.Voice;
 using Twilio.Types;
-using Task = System.Threading.Tasks.Task;
+
 public class TwilioWhatsAppSender : ITwilioWhatsAppSender
 {
 	private readonly IConfiguration _config;
@@ -15,17 +14,16 @@ public class TwilioWhatsAppSender : ITwilioWhatsAppSender
 	{
 		var sid = _config["Twilio:AccountSid"]!;
 		var token = _config["Twilio:AuthToken"]!;
-		var from = _config["Twilio:From"]!;
+		var from = _config["Twilio:WhatsAppFrom"]!;
 
 		TwilioClient.Init(sid, token);
 
 		var msg = MessageResource.Create(
 			from: new PhoneNumber(from),
-			to: new PhoneNumber(toE164),
+			to: new PhoneNumber("whatsapp:" + toE164),
 			body: text
 		);
 
 		return Task.FromResult(msg.Sid);
 	}
-
 }
